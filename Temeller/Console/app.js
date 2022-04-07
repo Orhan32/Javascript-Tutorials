4d5d073a95a6b3b00451f9ad74b6c7f16cd35383
Orhan Kalkan <45071936+Orhan32@users.noreply.github.com>
2022-04-08 00:55:23 +0300

    js 1

---
+++ Temeller/Console/app.js
@@ -0,0 +1,17 @@
+// console.log("Merhaba Javascript");
+// console.log(13);
+// console.log(true);
+// console.log(false);
+
+// var a = 12;
+
+// console.log(a);
+
+// console.log(["Ahmet","Mehmet"]);
+
+console.log(typeof a);
+
+console.warn("Uyarı");
+console.error("Hata");
+
+console.clear();
---
+++ Temeller/Degiskenler,VeriTipleri/app.js
@@ -0,0 +1,78 @@
+// Değişken Oluşturma
+// var a = 20;
+// var b = 10;
+// var c = 40;
+
+// console.log(a,b,c);
+
+// Primitive
+// var a = 10; // Number
+// var b = 9.99; // Number
+
+// console.log(typeof b);
+
+// var name = "Orhan"; // String
+
+// Boolean
+
+// var a = true; // Boolean
+// var b = false; // Boolean
+
+// console.log(typeof b);
+
+// var a = null; // Object
+
+// console.log(typeof a);
+
+// var a; // Undefined
+
+// console.log(a);
+
+// var numbers = [1,2,3,4,5]; // Array(Object)
+
+// console.log(numbers);
+// console.log(typeof numbers);
+// console.log(numbers[1]); // 2
+
+
+// var person = {
+//     name: "Orhan",
+//     surname: "Kalkan",
+//     age: "21"
+// }
+
+// console.log(person);
+// console.log(typeof person);
+
+
+// var date = new Date();
+
+// console.log(date);
+// console.log(typeof date);
+
+// var getir = function(){
+//     console.log("getiridi");
+// }
+
+// getir();
+
+
+
+// Referance Veri Tipleri
+
+// var a = 10;
+// var b = a;
+
+// console.log(a,b); // 10 10
+
+// a = 20;
+
+// console.log(a,b); // 20 10
+
+
+var a = [1,2,6]
+var b = a;
+
+a.push(4);
+
+console.log(b);
