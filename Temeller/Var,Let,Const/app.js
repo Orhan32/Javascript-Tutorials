2ad3891d19343c80c438da7940eaed5680bd98eb
Orhan Kalkan <45071936+Orhan32@users.noreply.github.com>
2022-04-08 02:35:14 +0300

    var, let, const

---
+++ Temeller/Var,Let,Const/app.js
@@ -0,0 +1,32 @@
+// Var
+
+// var name = "Orhan Kalkan";
+
+// name = "orhan";
+// console.log(name)
+
+
+// Let
+
+// let name = "Orhan Kalkan";
+// console.log(name);
+
+// name = "Orhan";
+// console.log(name);
+
+// let a, b;
+// a = 10;
+// b = 20;
+// console.log(a + b);
+
+// Const
+
+// const name = "Orhan Kakan";
+// console.log(name);
+
+// const a = 20;
+// console.log(a);
+
+const a = [1, 2, 3, 4, 5];
+a.push(6);
+console.log(a);
