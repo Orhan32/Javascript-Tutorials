b35ed53a4849ada72b000baa0bc33eca487238d6
Orhan Kalkan <45071936+Orhan32@users.noreply.github.com>
2022-04-09 02:54:04 +0300

    Window

---
+++ Temeller/Window Object/app.js
@@ -0,0 +1,46 @@
+// Alert
+
+// alert(1337);
+
+// Confirm
+
+// const cevap = confirm("Emin Misiniz?");
+// // console.log(cevap);
+
+// if (cevap) {
+//     console.log("Silindi");
+// }
+// else {
+//     console.log("İptal Edildi");
+// }
+
+// const cevap = prompt("2 + 2 = ?");
+
+// if (cevap == "4") {
+//     console.log("tamamdır");
+// }
+// else {
+//     console.log("yanlış");
+// }
+
+let value;
+value = window.location;
+value = window.location.host;
+value = window.location.hostname;
+value = window.location.port;
+value = window.location.href;
+
+// if (confirm("Yenilensin mi?")) {
+//     value = window.location.reload();
+// }
+
+value = window.outerHeight;
+value = window.outerWidth;
+
+value = window.innerHeight;
+value = window.innerWidth;
+
+value = window.scrollX;
+value = window.scrollY;
+
+console.log(value);
