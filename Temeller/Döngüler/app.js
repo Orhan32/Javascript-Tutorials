f6a8b84d3ae1b22eef15082d2a95dd896ee93460
Orhan Kalkan <45071936+Orhan32@users.noreply.github.com>
2022-04-09 02:35:59 +0300

    döngüler

---
+++ Temeller/Döngüler/app.js
@@ -0,0 +1,79 @@
+// While Döngüleri
+
+// let i = 1;
+// while (i <= 10) {
+//     console.log(i);
+//     i += 1; //i++;
+// }
+
+// let i = 10;
+// while (i >= 0) {
+//     console.log(i);
+//     i -= 2;
+// }
+
+// While Break ve Continue
+
+// let i = 0;
+// while (i < 10) {
+//     console.log(i);
+//     if (i == 5) {
+//         break;
+//     }
+
+//     i++;
+// }
+
+// Do While
+
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++;
+
+// } while (i <= 10);
+
+// const langs = ["Python", "JavaScript", "Java"];
+
+// var i = 0;
+// while (i < langs.length) {
+//     console.log(langs[i]);
+//     i++;
+// }
+
+// for (let i = 0; i < langs.length; i++) {
+//     console.log(langs[i]);
+// }
+
+// langs.forEach(function (lang, index) {
+//     console.log(lang, index);
+// });
+
+// langs.forEach(diller => {
+//     console.log(diller);
+// });
+
+// const users = [
+//     { name: "Orhan", age: 21 },
+//     { name: "Ahmet", age: 12 },
+//     { name: "Zeynep", age: 40 }
+
+// ];
+
+// const names = users.map(function (user) {
+//     return user.name;
+// });
+
+// const ages = users.map(user => { return user.age; })
+
+// console.log(names);
+// console.log(ages);
+
+// const user = {
+//     name: "Orhan",
+//     age: 21
+// }
+
+// for (let key in user) {
+//     console.log(key, user[key]);
+// }
