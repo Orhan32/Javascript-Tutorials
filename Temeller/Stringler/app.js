cf9c6db213f9f46bcfd0b2afd4243d2815b03310
Orhan Kalkan <45071936+Orhan32@users.noreply.github.com>
2022-04-08 17:50:38 +0300

    operatörler

---
+++ Temeller/Stringler/app.js
@@ -0,0 +1,38 @@
+let value;
+const value1 = 10;
+const value2 = 4;
+
+// Aritmetik Operatörller
+
+// value = value1 + value2;
+// value = value1 + value2;
+// value = value1 * value2;
+// value = value1 / value2;
+// value = value1 % value2;
+
+value = Math.PI;
+value = Math.E;
+
+value = Math.round(3.8);
+value = Math.round(3.3);
+
+value = Math.ceil(3.2);
+
+value = Math.floor(3.8);
+
+value = Math.sqrt(16);
+
+
+value = Math.abs(-10);
+
+value = Math.pow(8, 3);
+
+
+value = Math.max(10, - 1, 100);
+value = Math.min(10, - 1, 100);
+
+value = Math.random();
+value = parseInt(Math.random() * 20);
+
+
+console.log(value);
