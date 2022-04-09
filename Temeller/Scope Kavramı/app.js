0698878f74076cbedff8b67a4ed7b9c383bc6ff1
Orhan Kalkan <45071936+Orhan32@users.noreply.github.com>
2022-04-09 03:04:53 +0300

    scope kavramı

---
+++ Temeller/Scope Kavramı/app.js
@@ -0,0 +1,34 @@
+// Scope Kavramı
+
+var database = "123456";
+
+// Global Scope
+
+// var value1 = 10;
+// let value2 = 20;
+// const value3 = 30;
+
+// console.log(value1, value2, value3);
+// function a() {
+//     // Function Scope
+//     var value1 = 15;
+//     let value2 = 25
+//     const value3 = 35;
+//     console.log(value1, value2, value3);
+// }
+
+// a();
+
+// if (true) {
+//     // Block Scope
+//     var value1 = 15;
+//     let value2 = 25;
+//     const value3 = 35;
+//     console.log(value1, value2, value3);
+// }
+
+if (true) {
+    var database = "323232";
+}
+
+// console.log(value1, value2, value3);
